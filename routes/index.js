c7338d34204919387fd85e4cfa686d970aa4974a
jaimielogan <jaimielogan@gmail.com>
2016-08-30 18:36:39 -0600

    Initial Commit Simple Styling

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+var unirest = require('unirest');
+
+var key = "016bb440492c4390905f4f3221aa960a";
+var books;
+
+/* GET home page. */
+router.get('/books', function(req, res, next) {
+  unirest.get('http://api.nytimes.com/svc/books/v3/lists/hardcover-fiction.json?api-key=' + key)
+  .end(function(response){
+    res.end('Done');
+    books = response.body.results.books;
+  });
+  res.render('index', { title: 'Book Browsing App', books: books });
+});
+
+module.exports = router;
